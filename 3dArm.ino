7ff385b6cc183a827f511aaf04345e2f1b4648b5
robertmer4asd <94993306+robertmer4asd@users.noreply.github.com>
2022-05-28 10:42:59 +0300

    Update 3dArm.ino

--- 3dArm.ino
+++ 3dArm.ino
@@ -1,64 +1,33 @@
 #include <Servo.h>
 #define S0 2
-#define S1 3
+#define S1 12
 #define S2 4
 #define S3 5
 #define sensorOut 13
 Servo servo1, servo2, servo3, servo4, servo5;
-int var1 = 0,var2 = 0,var3 = 0,var4 = 0, var5 = 0;
 int redfrequency = 0;
 int greenfrequency = 0;
 int bluefrequency = 0;
 void setup() {
   Serial.begin(9600);
-  servo1.attach(8);
+  servo1.attach(3);
   servo2.attach(9);
   servo3.attach(10);
   servo4.attach(11);
-  servo5.attach(12);
+  servo5.attach(6);
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(sensorOut, INPUT);
-  pinMode(6,INPUT_PULLUP);
 
   digitalWrite(S0,HIGH);
   digitalWrite(S1,LOW);
 }
 
 void loop() {
-  cubeFull();
-}
-
-void servos(void){
-  var1 = analogRead(A3);
-  var1 = map(var1, 0, 1023, 0, 180);
-  servo1.write(var1);
-  var2 = analogRead(A0);
-  var2 = map(var2, 0, 1023, 0, 180);
-  servo2.write(var2);
-  var3 = analogRead(A1);
-  var3 = map(var3, 0, 1023, 0, 180);
-  servo3.write(var3);
-  var4 = analogRead(A2);
-  var4 = map(var4, 0, 1023, 0, 180);
-  servo4.write(var4);
-  var5 = digitalRead(6);
-  if(var5 == 1){
-    servo5.write(0);
-  }
-  else{
-    if(var5 == 0){
-      servo5.write(180);
-    }
-  }
-  Serial.println(servo5.read());
-}
-int cubeFull(void){
   servo5.write(0);
-  servo3.write(40);
-  delay(1000);
+  servo3.write(0);
   servo1.write(0);
   delay(1000);
   servo4.write(0);
@@ -70,21 +39,23 @@ int cubeFull(void){
   digitalWrite(S2,LOW);
   digitalWrite(S3,LOW);
   redfrequency = pulseIn(sensorOut, LOW);
+  redfrequency = map(redfrequency, 25,72,255,0);
  
   delay(100);
   digitalWrite(S2,HIGH);
   digitalWrite(S3,HIGH);
-  greenfrequency = pulseIn(sensorOut, LOW); 
+  greenfrequency = pulseIn(sensorOut, LOW);
+  greenfrequency = map(greenfrequency, 30,90,255,0);
   delay(100);
   digitalWrite(S2,LOW);
   digitalWrite(S3,HIGH);
   bluefrequency = pulseIn(sensorOut, LOW);
+  bluefrequency = map(bluefrequency, 25,70,255,0);
   delay(1000);
-  if (redfrequency>25 && redfrequency< 77) {
+  if (redfrequency >=0) {
     delay(1000);
     servo4.write(0);
     delay(1000);
-    servo3.write(0);
     for(int n = 0;n<=90;n++){
       delay(10);
       servo3.write(n);
@@ -97,19 +68,16 @@ int cubeFull(void){
     delay(3000);
     servo5.write(180);
     delay(1000);
-    servo4.write(90);
-    delay(1000);
-    servo3.write(0);
+    servo4.write(0);
     delay(1000);
     servo1.write(90);
     delay(1000);
     servo4.write(90);
     delay(1000);
-    servo3.write(90);
-    delay(1000);
     servo5.write(0);
+    delay(1000);
     }
-    else if (bluefrequency>25 && bluefrequency< 77) {
+    else if (bluefrequency>=0) {
       delay(1000);
       servo4.write(0);
       delay(1000);
@@ -131,8 +99,9 @@ int cubeFull(void){
       servo4.write(180);
       delay(1000);
       servo5.write(0);
+      delay(1000);
       } 
-    else if (greenfrequency>25 && greenfrequency< 77){
+    else if (greenfrequency>=0){
       Serial.println("GREEN COLOUR");
       }
 
@@ -165,5 +134,6 @@ int cubeFull(void){
       servo3.write(90);
       delay(1000);
       servo5.write(0);
+      delay(1000);
   }
 }
